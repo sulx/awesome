de7cadb133b46b58309ea35e12a9df826ee8e302
Sulx <jheikkal@gmail.com>
2012-11-18 11:32:13 +0200

    Error handling

--- rc.lua
+++ rc.lua
@@ -1,3 +1,5 @@
+-- Config by sulx
+
 -- Standard awesome library
 require("awful")
 require("awful.autofocus")
@@ -7,30 +9,31 @@ require("beautiful")
 -- Notification library
 require("naughty")
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
-end
-
--- Handle runtime errors after startup
+-- startup error
 do
-    local in_error = false
-    awesome.add_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
+  local in_error = false
+  awesome.add_signal("debug::error", function (err)
+    -- Make sure we don't go into an endless error loop
+    if in_error then return end
+    in_error = true
 
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = err })
-        in_error = false
-    end)
+    naughty.notify({ preset = naughty.config.presets.critical,
+    title = "time to debug",
+    text = err,
+    screen = mouse.screen,
+    timeout = 0 })
+    in_error = false
+  end)
 end
--- }}}
+
+
+-- error handling
+if awesome.startup_errors then
+  naughty.notify({ preset = naughty.config.presets.critical,
+  title = "Oops, there were errors during startup!",
+  screen = mouse.screen,
+  text = awesome.startup_errors })
+end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
